5963573349bcb57c09c5c94043603203e84823fb
ZechCodes <hi@zech.codes>
2024-02-01 14:45:37 -0500

    Add a test for the sqlite driver

---
+++ tests/test_sqlite_driver.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+import pytest
+
+from ommi.driver_context import use_driver
+from ommi.ext.drivers.sqlite import SQLiteConfig, SQLiteDriver
+from ommi.model_collections import ModelCollection
+from ommi.models import ommi_model
+from ommi.statuses import DatabaseStatus
+
+
+@pytest.mark.asyncio
+async def test_sqlite_driver():
+    models = ModelCollection()
+
+    @ommi_model(collection=models)
+    @dataclass
+    class DummyModel:
+        name: str
+
+    with use_driver(SQLiteDriver()) as driver:
+        await driver.connect(SQLiteConfig(filename=":memory:"))
+        await driver.sync_schema(models)
+
+        model = DummyModel(name="dummy")
+        await model.add()
+
+        result = await driver.fetch(DummyModel.name == "dummy")
+        assert isinstance(result, DatabaseStatus.Success)
+        assert result.value.name == model.name
