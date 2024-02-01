599b86e5bda87a93372ca7683c70337efc2bd485
ZechCodes <hi@zech.codes>
2024-02-01 14:37:29 -0500

    Fixed use_driver’s type hinting in PyCharm

--- ommi/driver_context.py
+++ ommi/driver_context.py
@@ -1,15 +1,27 @@
 from contextvars import ContextVar
 from contextlib import contextmanager
+from typing import Generator, TypeVar, Generic
 
 import ommi.drivers as drivers
+
+
+T = TypeVar("T", bound="drivers.DatabaseDriver")
 
 
 active_driver = ContextVar("active_driver")
 
 
-@contextmanager
-def use_driver(driver: "drivers.DatabaseDriver"):
-    previous_driver = active_driver.get()
-    active_driver.set(driver)
-    yield driver
-    active_driver.set(previous_driver)
+class UseDriver(Generic[T]):
+    def __init__(self, driver: T):
+        self.driver = driver
+        self._previous_context_token = None
+
+    def __enter__(self) -> T:
+        self._previous_context_token = active_driver.set(self.driver)
+        return self.driver
+
+    def __exit__(self, *_):
+        active_driver.reset(self._previous_context_token)
+
+def use_driver(driver: T) -> UseDriver[T]:
+    return UseDriver(driver)
