fe5f93204140301b8713671af19b4541aa70bdc0
ZechCodes <hi@zech.codes>
2024-04-23 13:25:34 -0400

    Test that model CRUD methods call the active driver

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,11 @@
 import dataclasses
 from typing import Annotated
+from unittest.mock import MagicMock
 
+import pytest
+
+from ommi.driver_context import use_driver
+from ommi.drivers import AbstractDatabaseDriver
 from ommi.field_metadata import create_metadata_flag
 from ommi.models import ommi_model, OmmiModel
 import attrs
@@ -10,6 +15,12 @@ from ommi.query_ast import ASTReferenceNode
 
 
 MetadataFlag = create_metadata_flag("MetadataFlag")
+
+
+@pytest.fixture
+def driver_mock():
+    with use_driver(MagicMock(spec=AbstractDatabaseDriver)) as mock_driver:
+        yield mock_driver
 
 
 def test_attrs_model():
@@ -64,3 +75,27 @@ def test_field_metadata():
 
     assert "foo" in TestModel.__ommi_metadata__.fields
     assert TestModel.__ommi_metadata__.fields["foo"].matches(MetadataFlag)
+
+
+@pytest.mark.asyncio
+async def test_model_fetch(driver_mock):
+    @ommi_model
+    class TestModel(pydantic.BaseModel):
+        foo: int
+
+    await TestModel.add()
+    driver_mock.add.assert_awaited_once()
+
+    await TestModel.count()
+    driver_mock.count.assert_awaited_once()
+
+    await TestModel.delete()
+    driver_mock.delete.assert_awaited_once()
+
+    await TestModel.fetch()
+    driver_mock.fetch.assert_awaited_once()
+
+    await TestModel.update()
+    driver_mock.update.assert_awaited_once()
+
+
