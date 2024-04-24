624fe66e59000e4611319585126e1e415b75e94b
ZechCodes <hi@zech.codes>
2024-04-24 14:02:04 -0400

    Add helpful package level imports

--- ommi/__init__.py
+++ ommi/__init__.py
@@ -1,2 +1,4 @@
 from ommi.drivers import DatabaseDriver
-from ommi.driver_context import active_driver
+from ommi.driver_context import active_driver, use_driver
+from ommi.models import ommi_model
+from ommi.field_metadata import Auto, Key, FieldType, FieldName
