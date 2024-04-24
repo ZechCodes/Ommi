2ecebf48881512ddf31ff880263654fc39a9a3a4
ZechCodes <hi@zech.codes>
2024-04-24 13:21:32 -0400

    Break up sqlite driver tests

--- tests/test_sqlite_driver.py
+++ tests/test_sqlite_driver.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import pytest
+import pytest_asyncio
 
 from ommi.driver_context import use_driver
 from ommi.ext.drivers.sqlite import SQLiteConfig, SQLiteDriver
@@ -9,23 +10,73 @@ from ommi.models import ommi_model
 from ommi.statuses import DatabaseStatus
 
 
-@pytest.mark.asyncio
-async def test_sqlite_driver():
-    models = ModelCollection()
+test_models = ModelCollection()
 
-    @ommi_model(collection=models)
-    @dataclass
-    class DummyModel:
-        name: str
-        id: int = None
 
+@ommi_model(collection=test_models)
+@dataclass
+class TestModel:
+    name: str
+    id: int = None
+
+
+@pytest.fixture
+def sqlite_driver() -> SQLiteDriver:
+    """Need to use a pytest fixture so that the contextvar exists in the same context as the test."""
     with use_driver(SQLiteDriver()) as driver:
-        await driver.connect(SQLiteConfig(filename=":memory:")).or_raise()
-        await driver.sync_schema(models).or_raise()
+        yield driver
 
-        model = DummyModel(name="dummy")
-        await model.add().or_raise()
 
-        result = await driver.fetch(DummyModel.name == "dummy").or_raise()
-        assert isinstance(result, DatabaseStatus.Success)
-        assert result.value[0].name == model.name
+@pytest_asyncio.fixture
+async def driver(sqlite_driver):
+    await sqlite_driver.connect(SQLiteConfig(filename=":memory:")).or_raise()
+    await sqlite_driver.sync_schema(test_models).or_raise()
+
+    yield sqlite_driver
+
+
+@pytest.mark.asyncio
+async def test_sqlite_driver(driver):
+    model = TestModel(name="dummy")
+    await model.add().or_raise()
+
+    result = await driver.fetch(TestModel.name == "dummy").or_raise()
+    assert result.value[0].name == model.name
+
+    model.name = "Dummy"
+    await model.sync().or_raise()
+    result = await driver.fetch(TestModel.name == "Dummy").or_raise()
+    assert result.value[0].name == model.name
+
+    await model.delete().or_raise()
+    result = await driver.fetch(TestModel.name == "Dummy").or_raise()
+    assert len(result.value) == 0
+
+
+@pytest.mark.asyncio
+async def test_sqlite_fetch(driver):
+    model = TestModel(name="dummy")
+    await model.add().or_raise()
+
+    result = await driver.fetch(TestModel.name == "dummy").or_raise()
+    assert result.value[0].name == model.name
+
+@pytest.mark.asyncio
+async def test_sqlite_update(driver):
+    model = TestModel(name="dummy")
+    await model.add().or_raise()
+
+    model.name = "Dummy"
+    await model.sync().or_raise()
+
+    result = await driver.fetch(TestModel.name == "Dummy").or_raise()
+    assert result.value[0].name == model.name
+
+@pytest.mark.asyncio
+async def test_sqlite_delete(driver):
+    model = TestModel(name="dummy")
+    await model.add().or_raise()
+
+    await model.delete().or_raise()
+    result = await driver.fetch(TestModel).or_raise()
+    assert len(result.value) == 0
