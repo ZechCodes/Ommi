4bbc7e39fb68f595329cf8acafd38556378433fb
ZechCodes <hi@zech.codes>
2024-05-22 17:00:10 -0400

    Give model collections a cleaner repr

--- ommi/model_collections.py
+++ ommi/model_collections.py
@@ -9,3 +9,6 @@ class ModelCollection:
 
     def add(self, model: "Type[ommi.models.OmmiModel]"):
         self.models.add(model)
+
+    def __repr__(self):
+        return f"<{type(self).__name__}: contains {len(self.models)} model{'' if len(self.models) == 1 else 's'}>"
