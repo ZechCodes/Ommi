08803285bc7ceba783fb68d746b4abc24477c4cb
ZechCodes <hi@zech.codes>
2024-05-30 22:31:07 -0400

    Cleanup tests

--- tests/test_query_fields.py
+++ tests/test_query_fields.py
@@ -12,6 +12,21 @@ from ommi.models.query_fields import LazyLoadTheRelated, LazyLoadEveryRelated
 from ommi.query_ast import search
 
 
+@ommi_model
+@dataclass
+class ModelA:
+    id: int
+
+@ommi_model
+@dataclass
+class ModelB:
+    id: int
+    a_id: Annotated[int, ReferenceTo(ModelA)]
+
+
+a = ModelA(id=1)
+
+
 @pytest.mark.asyncio
 @pytest.mark.parametrize("loader", [LazyLoadTheRelated, LazyLoadEveryRelated])
 async def test_load_relation(loader):
@@ -19,19 +34,6 @@ async def test_load_relation(loader):
         spec=AbstractDatabaseDriver,
         find=Mock(return_value=AsyncMock()),
     )
-
-    @ommi_model
-    @dataclass
-    class ModelA:
-        id: int
-
-    @ommi_model
-    @dataclass
-    class ModelB:
-        id: int
-        a_id: Annotated[int, ReferenceTo(ModelA)]
-
-    a = ModelA(id=1)
 
     relation = loader(search(ModelB.a_id == a.id), driver=driver_mock)
     result = await relation.result
@@ -51,19 +53,6 @@ async def test_load_relation_fails(loader):
         spec=AbstractDatabaseDriver,
         find=Mock(side_effect=Exception("Error")),
     )
-
-    @ommi_model
-    @dataclass
-    class ModelA:
-        id: int
-
-    @ommi_model
-    @dataclass
-    class ModelB:
-        id: int
-        a_id: Annotated[int, ReferenceTo(ModelA)]
-
-    a = ModelA(id=1)
 
     relation = loader(search(ModelB.a_id == a.id), driver=driver_mock)
     result = await relation.result
