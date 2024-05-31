402f0e372a64855e7b43f8bec2533c1ed4a84317
ZechCodes <hi@zech.codes>
2024-05-30 22:27:40 -0400

    Add a test for relation load failures

--- tests/test_query_fields.py
+++ tests/test_query_fields.py
@@ -36,8 +36,44 @@ async def test_load_relation(loader):
     relation = loader(search(ModelB.a_id == a.id), driver=driver_mock)
     result = await relation.result
     driver_mock.find.assert_called_with(ModelB.a_id == a.id)
+
     assert isinstance(result, Result.Value)
 
     await relation.value
     await relation.get()
     driver_mock.find.assert_called_once()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("loader", [LazyLoadTheRelated, LazyLoadEveryRelated])
+async def test_load_relation_fails(loader):
+    driver_mock = MagicMock(
+        spec=AbstractDatabaseDriver,
+        find=Mock(side_effect=Exception("Error")),
+    )
+
+    @ommi_model
+    @dataclass
+    class ModelA:
+        id: int
+
+    @ommi_model
+    @dataclass
+    class ModelB:
+        id: int
+        a_id: Annotated[int, ReferenceTo(ModelA)]
+
+    a = ModelA(id=1)
+
+    relation = loader(search(ModelB.a_id == a.id), driver=driver_mock)
+    result = await relation.result
+    driver_mock.find.assert_called_with(ModelB.a_id == a.id)
+
+    assert isinstance(result, Result.Error)
+
+    assert await relation.get("default") == "default"
+
+    with pytest.raises(Exception):
+        await relation.value
+
+    assert driver_mock.find.call_count == 3
