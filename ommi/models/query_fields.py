29f3d0a7deea9b79208fca8fb28a0902cae51cee
ZechCodes <hi@zech.codes>
2024-05-30 22:19:16 -0400

    Implement a lazy loader

---
+++ ommi/models/query_fields.py
@@ -0,0 +1,107 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+from tramp.results import Result
+
+from ommi import active_driver
+from ommi.drivers.drivers import AbstractDatabaseDriver
+from ommi.query_ast import search, ASTGroupNode
+
+T = TypeVar("T")
+
+
+class LazyQueryField(ABC):
+    def __init__(self, query: ASTGroupNode, driver: AbstractDatabaseDriver | None = None):
+        self._query = query
+        self._driver = driver
+
+        self._cache = Result.Error(ValueError("Not cached yet"))
+
+    def __await__(self):
+        return self.value.__await__()
+
+    @abstractmethod
+    async def get(self, default=None):
+        ...
+
+    async def refresh(self) -> None:
+        with Result.build() as builder:
+            builder.set(await self._fetch())
+
+        self._cache = builder.result
+
+    async def refresh_if_needed(self) -> None:
+        match self._cache:
+            case Result.Error():
+                await self.refresh()
+
+    @property
+    @abstractmethod
+    async def result(self):
+        ...
+
+    @property
+    @abstractmethod
+    async def value(self):
+        ...
+
+    @abstractmethod
+    async def _fetch(self):
+        ...
+
+    async def _get_result(self):
+        match self._cache:
+            case Result.Value() as result:
+                return result
+
+            case Result.Error():
+                self._cache = await self._fetch()
+                return self._cache
+
+    def _get_driver(self):
+        return self._driver or active_driver.get()
+
+
+class LazyLoadTheRelated(Generic[T], LazyQueryField):
+    async def get(self, default: T | None = None) -> T | None:
+        return (await self.result).value_or(default)
+
+    @property
+    async def result(self) -> Result[T]:
+        return await self._get_result()
+
+    @property
+    async def value(self) -> T:
+        return (await self.result).value
+
+    async def _fetch(self):
+        with Result.build() as builder:
+            builder.set(
+                await self._get_driver()
+                .find(self._query.limit(1))
+                .fetch.one()
+            )
+
+        return builder.result
+
+
+class LazyLoadEveryRelated(Generic[T], LazyQueryField):
+    async def get(self, default: list[T] | None = None) -> list[T] | None:
+        return (await self.result).value_or(default)
+
+    @property
+    async def result(self) -> Result[list[T]]:
+        return await self._get_result()
+
+    @property
+    async def value(self) -> list[T]:
+        return (await self.result).value
+
+    async def _fetch(self):
+        with Result.build() as builder:
+            builder.set(
+                await self._get_driver()
+                .find(self._query)
+                .fetch.all()
+            )
+
+        return builder.result
---
+++ tests/test_query_fields.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import Annotated
+from unittest.mock import MagicMock, AsyncMock, Mock
+
+import pytest
+from tramp.results import Result
+
+from ommi import ommi_model
+from ommi.drivers.drivers import AbstractDatabaseDriver
+from ommi.drivers.find_actions import FindAction
+from ommi.models.field_metadata import ReferenceTo
+from ommi.models.query_fields import LazyLoadTheRelated, LazyLoadEveryRelated
+from ommi.query_ast import search
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("loader", [LazyLoadTheRelated, LazyLoadEveryRelated])
+async def test_load_relation(loader):
+    driver_mock = MagicMock(
+        spec=AbstractDatabaseDriver,
+        find=Mock(
+            return_value=AsyncMock(
+                one=AsyncMock()
+            )
+        )
+    )
+
+    @ommi_model
+    @dataclass
+    class ModelA:
+        id: int
+
+    @ommi_model
+    @dataclass
+    class ModelB:
+        id: int
+        a_id: Annotated[int, ReferenceTo(ModelA)]
+
+    a = ModelA(id=1)
+
+    relation = loader(search(ModelB.a_id == a.id), driver=driver_mock)
+    result = await relation.result
+    driver_mock.find.assert_called_with(ModelB.a_id == a.id)
+    assert isinstance(result, Result.Value)
+
+    await relation.value
+    await relation.get()
+    driver_mock.find.assert_called_once()
