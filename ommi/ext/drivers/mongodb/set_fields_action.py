26029650ef0b13f79b5ffa102d1415e86de20a1f
ZechCodes <hi@zech.codes>
2024-06-04 13:44:24 -0400

    Cleanup set field’s pipeline

--- ommi/ext/drivers/mongodb/set_fields_action.py
+++ ommi/ext/drivers/mongodb/set_fields_action.py
@@ -3,7 +3,7 @@ from typing import Any, Sequence
 from ommi.drivers.database_results import async_result
 from ommi.drivers.set_fields_actions import SetFieldsAction
 from ommi.ext.drivers.mongodb.connection_protocol import MongoDBConnection
-from ommi.ext.drivers.mongodb.utils import build_pipeline, process_ast
+from ommi.ext.drivers.mongodb.utils import process_ast
 from ommi.models import OmmiModel
 from ommi.query_ast import when, ASTGroupNode
 
@@ -18,12 +18,13 @@ class MongoDBSetFieldsAction(SetFieldsAction[MongoDBConnection, OmmiModel]):
     @async_result
     async def set_fields(self, **kwargs: Any) -> bool:
         query = process_ast(when(*self._predicates))
-        pipeline, model = build_pipeline(query)
-        await self._db[model.__ommi_metadata__.model_name].update_many(
-            pipeline[0]["$match"],
+        await self._db[query.collection.__ommi_metadata__.model_name].update_many(
+            {
+                "$and": query.match,
+            },
             {
                 "$set": {
-                    model.__ommi_metadata__.fields[name].get("store_as"): value
+                    query.collection.__ommi_metadata__.fields[name].get("store_as"): value
                     for name, value in kwargs.items()
                 },
             },
