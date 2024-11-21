181eb0a80f3a7626030741cd6121c216ed020e2d
ZechCodes <hi@zech.codes>
2024-11-21 16:09:04 -0500

    Use type aliases for SQLite schema management

--- ommi/ext/drivers/sqlite/schema_management.py
+++ ommi/ext/drivers/sqlite/schema_management.py
@@ -3,28 +3,28 @@ from typing import Generator, TYPE_CHECKING
 from ommi.ext.drivers.sqlite.utils import get_sqlite_type
 
 if TYPE_CHECKING:
-    import sqlite3
-    from ommi.models import OmmiModel
+    from ommi.ext.drivers.sqlite.shared_types import Cursor, SQLStatement
     from ommi.models.collections import ModelCollection
     from ommi.models.field_metadata import FieldMetadata
+    from ommi.shared_types import DBModel
 
 
-async def apply_schema(cursor: "sqlite3.Cursor", models: "ModelCollection"):
+async def apply_schema(cursor: "Cursor", models: "ModelCollection"):
     for create_table_sql in _generate_create_table_sql(models):
-        cursor.execute(create_table_sql)
+        cursor.execute(create_table_sql, ())
 
 
-async def delete_schema(cursor: "sqlite3.Cursor", models: "ModelCollection"):
+async def delete_schema(cursor: "Cursor", models: "ModelCollection"):
     for drop_table_sql in _generate_drop_table_sql(models):
-        cursor.execute(drop_table_sql)
+        cursor.execute(drop_table_sql, ())
 
 
-def _generate_create_table_sql(collection: "ModelCollection") -> Generator[str, None, None]:
+def _generate_create_table_sql(collection: "ModelCollection") -> "Generator[SQLStatement, None, None]":
     for model in collection.models:
         yield _generate_create_table_sql_for_model(model)
 
 
-def _generate_create_table_sql_for_model(model: "OmmiModel") -> str:
+def _generate_create_table_sql_for_model(model: "DBModel") -> "SQLStatement":
     return (
         f"CREATE TABLE IF NOT EXISTS {model.__ommi__.model_name}"
         f"("
@@ -33,18 +33,18 @@ def _generate_create_table_sql_for_model(model: "OmmiModel") -> str:
         f");"
     )
 
-def _generate_primary_keys_sql(model: "OmmiModel") -> str:
+def _generate_primary_keys_sql(model: "DBModel") -> "SQLStatement":
     return ", ".join(
         pk.get("store_as") for pk in model.get_primary_key_fields()
     )
 
-def _generate_columns_sql(model: "OmmiModel") -> str:
+def _generate_columns_sql(model: "DBModel") -> "SQLStatement":
     return ", ".join(
         _generate_column_sql(field) for field in model.__ommi__.fields.values()
     )
 
 
-def _generate_column_sql(field: "FieldMetadata") -> str:
+def _generate_column_sql(field: "FieldMetadata") -> "SQLStatement":
     return " ".join(
         (
             field.get("store_as"),
@@ -53,6 +53,6 @@ def _generate_column_sql(field: "FieldMetadata") -> str:
     )
 
 
-def _generate_drop_table_sql(collection: "ModelCollection") -> Generator[str, None, None]:
+def _generate_drop_table_sql(collection: "ModelCollection") -> "Generator[SQLStatement, None, None]":
     for model in collection.models:
         yield f"DROP TABLE IF EXISTS {model.__ommi__.model_name};"
