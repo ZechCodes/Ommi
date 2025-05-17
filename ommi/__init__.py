0ba6e62a3433c604df3d1f279ac8482920392a36
ZechCodes <hi@zech.codes>
2025-05-17 17:33:16 -0400

    Rework the top level package imports to avoid circular imports

--- ommi/__init__.py
+++ ommi/__init__.py
@@ -1,4 +1,66 @@
-from ommi.drivers import BaseDriver, BaseDriverTransaction
-from ommi.driver_context import active_driver, use_driver
-from ommi.models import ommi_model
-from ommi.models.field_metadata import Auto, Key, FieldType, StoreAs
+import importlib
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ommi.drivers import BaseDriver, BaseDriverTransaction
+    from ommi.driver_context import active_driver, use_driver
+    from ommi.models import ommi_model
+    from ommi.models.field_metadata import Auto, Key, FieldType, StoreAs
+    from ommi.database import Ommi
+
+__all__ = [
+    "ommi_model",
+    "Auto",
+    "Key",
+    "FieldType",
+    "StoreAs",
+    "Ommi",
+    "active_driver",
+    "use_driver",
+    "BaseDriver",
+    "BaseDriverTransaction",
+]
+
+
+__lookup = {
+    "BaseDriver": "ommi.drivers",
+    "BaseDriverTransaction": "ommi.drivers",
+    "active_driver": "ommi.driver_context",
+    "use_driver": "ommi.driver_context",
+    "Ommi": "ommi.database",
+    "ommi_model": "ommi.models",
+    "FieldType": "ommi.models.field_metadata",
+    "StoreAs": "ommi.models.field_metadata",
+    "Auto": "ommi.models.field_metadata",
+    "Key": "ommi.models.field_metadata",
+}
+
+__modules = set()
+
+# Iterate the local folder and search for all py files and folders
+for _path in Path(__file__).parent.iterdir():
+    if _path.name.startswith("_"):
+        continue
+
+    if not _path.is_dir() and _path.suffix != ".py":
+        continue
+
+    __modules.add(_path.stem)
+
+
+def __getattr__(name):
+    if name in __lookup:
+        print("Importing from ", __lookup[name])
+        try:
+            module = importlib.import_module(__lookup[name])
+        except Exception as e:
+            print(e)
+            raise ImportError(f"Failed to import {name} from {__lookup[name]}: {e}") from e
+        print(module)
+        return getattr(module, name)
+
+    if name in __modules:
+        return importlib.import_module(f"ommi.{name}")
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
