cdcc0e3c48c110e791772d7369e5995a205c9982
ZechCodes <hi@zech.codes>
2025-05-18 15:15:49 -0400

    Make transactions easier to import

--- ommi/database/__init__.py
+++ ommi/database/__init__.py
@@ -1,6 +1,7 @@
 from ommi.database.ommi import Ommi
 from ommi.database.results import DBResult
 from ommi.database.query_results import DBQueryResult
+from ommi.database.transaction import OmmiTransaction
 
 
-__all__ = ["Ommi", "DBResult", "DBQueryResult"]
+__all__ = ["Ommi", "DBResult", "DBQueryResult", "OmmiTransaction"]
